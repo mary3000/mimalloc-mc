2e99c34d12f15eb498fa5ee9c01f85571999abab
Mary Feofanova <mary.fifa3000@gmail.com>
2020-08-25 22:20:15 +0300

    1) more comments 2) bug flag

--- test/bitmap/main.c
+++ test/bitmap/main.c
@@ -10,27 +10,37 @@
 
 void __VERIFIER_assume(int);
 
-atomic_int field = ATOMIC_VAR_INIT(0);
+atomic_int bitmap = ATOMIC_VAR_INIT(0);
 
 atomic_int info = ATOMIC_VAR_INIT(0);
 
 void* thread_one(void* arg) {
-  atomic_store_explicit(&field, 0, memory_order_release);
+  // 1. Initialize bitmap
+  atomic_store_explicit(&bitmap, 0, memory_order_release);
+  // 2. Publish region
   atomic_store_explicit(&info, 1, memory_order_release);
 
   return NULL;
 }
 
 void* thread_two(void* arg) {
-  int info_val = atomic_load_explicit(&info, memory_order_relaxed); // should be acq!
+  // 1. Read info and assume another thread already published the region
+#if defined(GENMC_BUG_BITMAP)
+  int info_val = atomic_load_explicit(&info, memory_order_relaxed); // should be acquire!
+#else
+  int info_val = atomic_load_explicit(&info, memory_order_acquire);
+#endif
   __VERIFIER_assume(info_val == 1);
 
+  // 2. Claim bits in bitmap
   int exp = 0;
-  _Bool success = atomic_compare_exchange_weak_explicit(&field, &exp, 1, memory_order_acq_rel, memory_order_acquire);
+  _Bool success = atomic_compare_exchange_weak_explicit(&bitmap, &exp, 1, memory_order_acq_rel, memory_order_acquire);
   __VERIFIER_assume(success);
 
-  int field_val = atomic_load_explicit(&field, memory_order_relaxed);
-  assert(field_val == 1);
+  // 3. Read bitmap again in order to check if bits are claimed
+  int bitmap_val = atomic_load_explicit(&bitmap, memory_order_relaxed);
+  // assertion fails because there is no guarantee bitmap was read from T2's CAS, instead it can read from T1's store.
+  assert(bitmap_val == 1);
 
   return NULL;
 }
